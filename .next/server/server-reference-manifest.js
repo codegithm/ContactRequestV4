bcf18c860048218de7b321c27cd75b84d96051cf
Ntandoyenkosi Xakaza <xakazantando@gmail.com>
2026-04-09 08:55:40 +0200

    Remove tracked .next artifacts and ignore .next

--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"eJuvy8p1NAX/ikwdRU0ezMVBeyDK7CDQlTQ9kqxoQRo=\"\n}"
--- .next/static/chunks/src_app_[partnerId]_feedback_page_tsx_2ea00eae._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_698116a2._.js",
-  "static/chunks/node_modules_a246fdac._.js"
-],
-    source: "dynamic"
-});
